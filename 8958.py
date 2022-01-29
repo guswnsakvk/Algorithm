16a6f4de4f204d3eb57406e7072fd0da1da735f6
guswnsakvk <guswnsakvk1@naver.com>
2022-01-29 11:13:50 +0900

    8958

---
+++ 8958.py
@@ -0,0 +1,16 @@
+from re import I
+
+
+n = int(input())
+case = [ [] for i in range(n)]
+for i in range(n):
+  result = 0
+  sum = 0
+  case[i] = input()
+  for j in range(len(case[i])):
+    if case[i][j] == "O":
+      sum += 1
+      result += sum
+    else:
+      sum = 0
+  print(result)
