e1d7d886b9b857b6973bb93dfb7a861d39d7e158
guswnsakvk <guswnsakvk1@naver.com>
2022-04-14 14:30:16 +0900

    정렬 사용

--- programmers/전화번호_목록.py
+++ programmers/전화번호_목록.py
@@ -1,8 +1,8 @@
 def solution(phone_book):
     phone_book.sort()
     for i in range(len(phone_book)-1):
-        for j in range(i+1, len(phone_book)):
-            if phone_book[i] == phone_book[j][:len(phone_book[i])]:
+        if len(phone_book[i]) < len(phone_book[i+1]):
+            if phone_book[i+1][:len(phone_book[i])] == phone_book[i]:
                 return False
-            
+
     return True
