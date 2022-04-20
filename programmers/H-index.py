6b94b5a98719758685d78d953da2df9e77206ca7
guswnsakvk <guswnsakvk1@naver.com>
2022-04-20 14:39:35 +0900

    실패

--- programmers/H-index.py
+++ programmers/H-index.py
@@ -1,8 +1,6 @@
 def solution(citations):
-  answer = []
-  citations.sort()
-  for i in range(len(citations) // 2, len(citations)):
-      if len(citations[:i]) <= citations[i] and len(citations[i:]) >= citations[i]:
-          answer.append(citations[i])
-          
-  return answer[-1]
+  citations.sort(reverse=True)
+  for i, citation in enumerate(citations, start=1):
+      if citation <= i:
+          return citation
+  return 0
