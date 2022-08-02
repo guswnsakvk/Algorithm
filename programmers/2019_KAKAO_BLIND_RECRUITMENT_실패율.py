7a31e178a5e344417eaca7e69018377b6f745327
guswnsakvk <guswnsakvk1@naver.com>
2022-08-02 16:18:06 +0900

    값을 처음부터 초기화X

--- programmers/2019_KAKAO_BLIND_RECRUITMENT_실패율.py
+++ programmers/2019_KAKAO_BLIND_RECRUITMENT_실패율.py
@@ -1,9 +1,13 @@
 def solution(N, stages):
-  player = len(stages)
-  dictionary = {key+1 : 0 for key in range(N)}
-  for i in range(N):
-      cnt = stages.count(i+1)
-      dictionary[i+1] = cnt / player
-      player -= cnt
-  dictionary = dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))
-  return list(dictionary.keys())
+    answer = []
+    player = len(stages)
+    dictionary={}
+    for i in range(N):
+        cnt = stages.count(i+1)
+        if player == 0:
+            dictionary[i+1] = 0
+        else:
+            dictionary[i+1] = cnt / player
+        player -= cnt
+    answer = [stage[0] for stage in sorted(dictionary.items(), key=lambda x: x[1], reverse=True)]
+    return answer
