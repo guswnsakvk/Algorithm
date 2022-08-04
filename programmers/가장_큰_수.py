de6aea628d62b06932293bd9f9a93f18436dc0d9
guswnsakvk <guswnsakvk1@naver.com>
2022-08-04 15:30:10 +0900

    성공

--- programmers/가장_큰_수.py
+++ programmers/가장_큰_수.py
@@ -1,10 +1,6 @@
 def solution(numbers):
-    new_numbers = []
-    for i in range(len(numbers)):
-        numbers_copy = numbers[1:].copy()
-        for back in range(1, len(numbers)):
-            new_numbers.append(str(numbers[0]) + "".join(map(str, numbers_copy)))
-            numbers_copy.append(numbers_copy.pop(0))
-        numbers.append(numbers.pop(0))
-            
-    return str(max(map(int, new_numbers)))
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    answer = str(int("".join(numbers)))
+    
+    return answer
