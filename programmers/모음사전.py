270d7e8b5200d0b58f1ce85ea8c41922f27895f5
guswnsakvk <guswnsakvk1@naver.com>
2022-08-10 14:53:28 +0900

    중복순역 활용

--- programmers/모음사전.py
+++ programmers/모음사전.py
@@ -1,3 +1,4 @@
+# 순열
 from itertools import permutations
 
 def solution(word):
@@ -6,6 +7,24 @@ def solution(word):
     
     for i in range(5):
         new_dict = map("".join, permutations(vowel, i+1))
+        dict_vowel |= set(new_dict)
+    
+    dict_vowel = sorted(dict_vowel)
+    
+    for i in range(len(dict_vowel)):
+        if word == dict_vowel[i]:
+            return i+1
+
+
+# 중복순열
+from itertools import product
+
+def solution(word):
+    dict_vowel = set()
+    vowel = ['A', 'E', 'I', 'O', 'U']
+    
+    for i in range(5):
+        new_dict = map("".join, product(vowel, repeat = i+1))
         dict_vowel |= set(new_dict)
     
     dict_vowel = sorted(dict_vowel)
