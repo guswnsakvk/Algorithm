44c6939cd3224abf757912e122e6a6e89d3d0bec
guswnsakvk <guswnsakvk1@naver.com>
2023-03-08 13:37:02 +0900

    dfs로 구현하니깐 런타임 에러 뜸

---
+++ BackJoon/11724.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+answer = 0
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(graph, i, visited)
+        answer += 1
+
+print(answer)
