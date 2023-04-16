7cc049d10ab2738ae91508870dec191469ccfae3
guswnsakvk <guswnsakvk1@naver.com>
2023-04-16 22:56:18 +0900

    성공

--- programmers/거리두기_확인하기.py
+++ programmers/거리두기_확인하기.py
@@ -1,88 +1,35 @@
+## 맨헤튼 거리가 2인 걸 모든 경우의 수를 확인하려면 너무 조건이 복잡해짐
+## 처음에 코드를 짤때 모든 경우의 수를 구하려고 하니 너무 지저분했음
+## 현재 위치가 P이면 주변에 P가 1개 이상이면 거리두기를 실패한 것이고
+## O이면 주변에 P가 2개 이상이면 거리두기를 실패한 것이다.
+
 def solution(places):
     answer = [1] * len(places)
-    for cnt in range(len(places)):
-        flag = False
+    
+    for place in range(len(places)):
+        tmp = places[place]
         for i in range(5):
             for j in range(5):
-                if places[cnt][i][j] == 'P':
-                    if j - 1 > -1:
-                        if places[cnt][i][j-1] == 'P':
-                            flag = True
-                            break
-                        
-                    if j + 1 < 5:
-                        if places[cnt][i][j+1] == 'P':
-                            flag = True
-                            break
-                            
+                cnt = 0
+                if tmp[i][j] != 'X':
                     if i - 1 > -1:
-                        if places[cnt][i-1][j] == 'P':
-                            flag = True
-                            break
-                    
+                        if tmp[i-1][j] == 'P':
+                            cnt += 1
                     if i + 1 < 5:
-                        if places[cnt][i+1][j] == 'P':
-                            flag = True
-                            break
+                        if tmp[i+1][j] == 'P':
+                            cnt += 1
+                    if j - 1 > -1:
+                        if tmp[i][j-1] == 'P':
+                            cnt += 1
+                    if j + 1 < 5:
+                        if tmp[i][j+1] == 'P':
+                            cnt += 1
+                            
+                    if tmp[i][j] == 'P':
+                        if cnt >= 1:
+                            answer[place] = 0
+                    elif tmp[i][j] == 'O':
+                        if cnt >= 2:
+                            answer[place] = 0
                     
-                    if j - 2 > -1:
-                        if places[cnt][i][j-2] == 'P' and places[cnt][i][j-1] == 'X':
-                            continue
-                        else:
-                            flag = True
-                            break
-                        
-                    if j + 2 < 5:
-                        if places[cnt][i][j+2] == 'P' and places[cnt][i][j+1] == 'X':
-                            continue
-                        else:
-                            flag = True
-                            break
-                        
-                    if i - 2 > -1:
-                        if places[cnt][i-2][j] == 'P' and places[cnt][i-1][j] == 'X':
-                            continue
-                        else:
-                            flag = True
-                            break
-                            
-                    if i + 2 < 5:
-                        if places[cnt][i+2][j] == 'P' and places[cnt][i+1][j] == 'X':
-                            continue
-                        else:
-                            flag = True
-                            break
-                            
-                    if i - 1 > -1 and j - 1 > -1:
-                        if places[cnt][i-1][j-1] == 'P' and (places[cnt][i][j-1] == 'X' or places[cnt][i-1][j] == 'X'):
-                            continue
-                        else:
-                            flag = True
-                            break
-                            
-                    if i - 1 > -1 and j + 1 < 5:
-                        if places[cnt][i-1][j+1] == 'P' and (places[cnt][i][j+1] == 'X' or places[cnt][i-1][j] == 'X'):
-                            continue
-                        else:
-                            flag = True
-                            break
-                            
-                    if i + 1 > 5 and j - 1 < -1:
-                        if places[cnt][i+1][j-1] == 'P' and (places[cnt][i][j-1] == 'X' or places[cnt][i+1][j] == 'X'):
-                            continue
-                        else:
-                            flag = True
-                            break
-                            
-                    if i + 1 > 5 and j + 1 < 5:
-                        if places[cnt][i+1][j+1] == 'P' and (places[cnt][i][j+1] == 'X' or places[cnt][i+1][j] == 'X'):
-                            continue
-                        else:
-                            flag = True
-                            break
-                        
-            if flag:
-                answer[cnt] = 0
-                break
-
     return answer
