b9f0b564926ae1cfa64e7553a640935c315c39f9
guswnsakvk <guswnsakvk1@naver.com>
2023-04-28 16:09:46 +0900

    성공

--- programmers/광물_캐기.py
+++ programmers/광물_캐기.py
@@ -1,18 +1,38 @@
+"""
+1. 광물의 개수에 따른 필요한 곡괭이 갯수 구하기
+1) 5로 나누어 떨어질 경우는 len(minerals) // 5
+2) 5로 나누어 떨어지지 않는 경우는 len(minerals) // 5 + 1
+
+2. 필요한 곡괭이 갯수만큼 가장 좋은 곡괭이 고르기
+
+3. 순열을 사용해서 곡괭이를 사용하는 순서의 모든 경우의 수 구하기
+1) permutations 사용
+
+4. 모든 경우의 수마다 피로도를 구하기
+"""
+
+
 from itertools import permutations
 
 def solution(picks, minerals):
     pickaxs = []
     answer = 1e9
     
+    need_pick = len(minerals) // 5 if len(minerals) % 5 == 0 else len(minerals) // 5 + 1
+    
     for i in range(3):
-        for j in range(picks[i]):
-            pickaxs.append(i)
+        if need_pick != 0:
+            if picks[i] > need_pick:
+                pickaxs.extend([i] * need_pick)
+            else:
+                pickaxs.extend([i] * picks[i])
+            need_pick -= picks[i]
+        else:
+            break
     
-    per_num = min(len(minerals) // 5 if len(minerals) % 5 == 0 else len(minerals) // 5 + 1, len(pickaxs))
+    pickax = permutations(pickaxs, len(pickaxs))
     
-    pickax = permutations(pickaxs, per_num)
-    
-    for pick in pickax:
+    for pick in set(pickax):
         fatigue = 0
         cnt = 0
         l = 0
@@ -45,7 +65,6 @@ def solution(picks, minerals):
                         l += 1
                         cnt = 0
             except:
-                answer = min(answer, fatigue)
                 break
             
         answer = min(answer, fatigue)
