a1eadc3d45801e880bba3adcd710c2713cc7183a
guswnsakvk <guswnsakvk1@naver.com>
2023-05-01 13:47:49 +0900

    시간초과

--- programmers/리코쳇_로봇.py
+++ programmers/리코쳇_로봇.py
@@ -1,12 +1,17 @@
 from collections import deque
+import copy
 
 def solution(board):
     x_len = len(board)
     y_len = len(board[0])
+    
     start = find_R(board, x_len, y_len)
-    target = find_G(board, x_len, y_len)
-    answer = bfs(board, start, target, x_len, y_len)
-    print(start, target)
+    
+    visited = [[False for j in range(y_len)] for i in range(x_len)]
+    visited[start[0]][start[1]] = True
+    
+    answer = bfs(board, start, x_len, y_len, visited)
+
     return answer
 
 def find_R(board, x_len, y_len):
@@ -15,30 +20,22 @@ def find_R(board, x_len, y_len):
             if board[i][j] == "R":
                 return [i, j]
             
-def find_G(board, x_len, y_len):
-    for i in range(x_len):
-        for j in range(y_len):
-            if board[i][j] == "G":
-                return [i, j]
             
-def bfs(board, start, target, x_len, y_len):
-    visited = [[False for j in range(y_len)] for i in range(x_len)]
-    visited[start[0]][start[1]] = True
-    
+def bfs(board, start, x_len, y_len, visited):
     move = 1e9
 
     queue = deque()
-    queue.append((start[0], start[1], 0))
+    queue.append((start[0], start[1], 0, visited))
     
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
     
     while queue:
-        x, y, cnt = queue.popleft()
-        
-        print(x, y, cnt)
+        x, y, cnt, tmp = queue.popleft()
         
         for i in range(4):
+            check = copy.deepcopy(tmp)
+            
             nx = x + dx[i]
             ny = y + dy[i]
             
@@ -48,39 +45,42 @@ def bfs(board, start, target, x_len, y_len):
             if board[nx][ny] == "D":
                 continue
                 
-            if visited[nx][ny]:
-                continue
-                
             while True:
-                visited[nx][ny] = True
                 nx += dx[i]
                 ny += dy[i]
                 
                 if nx < 0 or ny < 0 or nx >= x_len or ny >= y_len:
                     nx -= dx[i]
                     ny -= dy[i]
+                    check[nx][ny] = True
                     print(nx, ny)
+                    print(check)
                     
                     if board[nx][ny] == "G":
                         move = min(move, cnt+1)
                     else:
-                        queue.append((nx, ny, cnt+1))
+                        queue.append((nx, ny, cnt+1, check))
                     
                     break
                     
+                if check[nx][ny] == True:
+                    break
+                    
                 if board[nx][ny] == "D":
+                    check[nx][ny] = True
                     nx -= dx[i]
                     ny -= dy[i]
                     print(nx, ny)
+                    print(check)
             
                     if board[nx][ny] == "G":
                         move = min(move, cnt+1)
                     else:
-                        queue.append((nx, ny, cnt+1))
+                        queue.append((nx, ny, cnt+1, check))
                     
                     break
             
         print('---')
-    # print(visited)
+    print(visited)
     
     return move if move != 1e9 else -1
