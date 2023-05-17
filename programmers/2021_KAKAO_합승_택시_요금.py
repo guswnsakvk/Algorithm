d1525116d3613b059054346a7690eb8977cfdd1f
guswnsakvk <guswnsakvk1@naver.com>
2023-05-17 14:43:58 +0900

    플로이드 사용

--- programmers/2021_KAKAO_합승_택시_요금.py
+++ programmers/2021_KAKAO_합승_택시_요금.py
@@ -1,52 +1,42 @@
-from collections import deque
+"""
+플로이드 알고리즘
+1. 모든 노드 간 최단 경로를 구하는 알고리즘
+2. 각 경로에서 새로운 중간 노드로 사용할 수 있는 노드를 선택하고
+   더 짧은 길이를 선택하여 줄이는 과정을 반복
+   ex) 1 -> 4 있다면 1 -> 2와 2 -> 4 둘을 합쳐서 더 짧은 길이를 선택
+3. 초기값은 자기 자신을 제외한 모든 노드 값을 INF로 초기화
+"""
+
+INF = 40000000
 
 def solution(n, s, a, b, fares):
-    answer = 1e9
-    graph = [[] for _ in range(n+1)]
-    fees = {}
+    answer = INF
     
-    for fare in fares:
-        start, end, fee = fare
-        graph[start].append(end)
-        graph[end].append(start)
-        fees[str(start) + str(end)] = fee
-        fees[str(end) + str(start)] = fee
+    dist = [[INF for _ in range(n)] for _ in range(n)]
     
-    for i in range(1, n+1):
-        share = dfs(n, s, i, graph, fees)
-        # print(share)
-        
-        if share[0] != 1e9:
-            for j in share[1]:
-                share_to_a = dfs(n, j, a, graph, fees)
-                share_to_b = dfs(n, j, b, graph, fees)
-                answer = min(answer, share[0] + share_to_a[0] + share_to_b[0])
-            # print(share_to_a, share_to_b)
-            # print('---')
+    for i in range(n):
+        dist[i][i] = 0
+    
+    for edge in fares:
+        dist[edge[0]-1][edge[1]-1] = edge[2]
+        dist[edge[1]-1][edge[0]-1] = edge[2]
+                
+    floyd(dist, n)
+    s -= 1
+    a -= 1
+    b -= 1
+    
+    for k in range(n):
+        answer = min(answer, dist[s][k] + dist[k][a] + dist[k][b])
     
     return answer
 
-def dfs(n, s, e, graph, fees):
-    path = 1e9
-    tmp = []
-    
-    visited = [False for _ in range(n+1)]
-    visited[s] = True
-    
-    queue = deque()
-    queue.append((s, 0))
-    
-    while queue:
-        x, num = queue.popleft()
-        
-        if x == e and path > num:
-            path = num
-            tmp.append(x)
-            continue
-        
-        for g in graph[x]:
-            if not visited[g]:
-                visited[g] = True
-                queue.append((g, num + fees[str(x) + str(g)]))
-    
-    return path, tmp
+def floyd(dist, n):
+    ## 중간 노드
+    for i in range(n):
+        ## 시작 노드
+        for j in range(n):
+            ## 끝 노드
+            for k in range(n):
+                if dist[j][i] + dist[i][k] < dist[j][k]:
+                    dist[j][k] = dist[j][i] + dist[i][k]
