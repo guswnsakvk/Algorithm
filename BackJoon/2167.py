2e0eb2eb4a3f114a028f6862312a70d7d0cf08ae
guswnsakvk <guswnsakvk1@naver.com>
2023-05-26 18:10:03 +0900

    2차원 배열의 합 : dp, 누적합 사용

--- BackJoon/2167.py
+++ BackJoon/2167.py
@@ -1,42 +1,17 @@
 n, m = map(int, input().split())
 
 arr = []
+dp = [[0 for _ in range(m+1)] for _ in range(n + 1)]
 
-for i in range(n):
+for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-for i in range(len(arr)):
-    for j in range(1, len(arr[0])):
-        arr[i][j] += arr[i][j-1]
-
-for i in range(len(arr[0])):
-    for j in range(1, len(arr)):
-        arr[j][i] += arr[j-1][i]
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
 
 k = int(input())
 
-for i in arr:
-    print(i)
-
 for _ in range(k):
-    answer = 0
     start_x, start_y, end_x, end_y = map(int, input().split())
-
-    if start_x == end_x and start_y == end_y:
-        if end_x == 1:
-            if end_y == 1:
-                print(arr[0][0])
-            else:
-              print(arr[end_x-1][end_y-1] - arr[end_x-1][end_y-2])
-        else:
-            if end_y == 1:
-                print(arr[end_x-1][end_y-1] - arr[end_x-2][end_y-1])
-            else:
-                print(arr[end_x-1][end_y-1] - arr[end_x-1][end_y-2] - arr[end_x-2][end_y-1] + arr[end_x-2][end_y-2])
-    elif start_x == 1 and start_y == 1:
-        print(arr[end_x-1][end_y-1])
-    else:
-        if start_x == end_x:
-            print(arr[end_x-1][end_y-1] - arr[end_x-2][end_y-1] - arr[end_x-1][start_y-2] + arr[end_x-2][start_y-2])
-        else:
-            print(arr[end_x-1][end_y-1] - arr[end_x-1][start_y-2])
+    print(dp[end_x][end_y] - dp[end_x][start_y-1] - dp[start_x-1][end_y] + dp[start_x-1][start_y-1])
