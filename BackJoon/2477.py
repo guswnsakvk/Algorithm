f1b51ea1f7fd19a247106380ba56bfca493cae4a
guswnsakvk <guswnsakvk1@naver.com>
2023-06-04 13:32:03 +0900

    실패

--- BackJoon/2477.py
+++ BackJoon/2477.py
@@ -1,21 +1,30 @@
 k = int(input())
 
 bulid = {1 : [], 2 : [], 3 : [], 4 : []}
+start = 0
+a = 0
+width = 0
+height = 0
+
 
 for _ in range(6):
     direction, length = map(int, input().split())
     bulid[direction].append(length)
+    if start == 0:
+        start = direction
+    if len(bulid[direction]) == 2 and a == 0 and start == direction:
+        width = bulid[direction][0]
+        a = direction
+    elif len(bulid[direction]) == 2 and a == 0:
+        width = length
+        a = direction
 
 max_width = sum(bulid[1])
 max_height = sum(bulid[3])
-width = 0
-height = 0
 
 for i in range(4):
-    if len(bulid[i+1]) == 2 and height == 0:
+    if len(bulid[i+1]) == 2 and a != i + 1:
         height = bulid[i+1][0]
-    elif len(bulid[i+1]) == 2:
-        width = bulid[i+1][1]
 
 print(max_width, max_height)
 print(width, height)
