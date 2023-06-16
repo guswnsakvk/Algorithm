049222ce65fad94ded71dd2e94659635ff7fd970
guswnsakvk <guswnsakvk1@naver.com>
2023-06-16 15:12:49 +0900

    시간 초과

--- BackJoon/10973.py
+++ BackJoon/10973.py
@@ -2,15 +2,17 @@ from itertools import permutations
 
 n = int(input())
 
-now = " ".join(input().split())
-prev = " ".join(map(str, range(1,n+1)))
+#now = tuple(input().split())
+#prev = tuple(map(str, range(1,n+1)))
 
-if now == prev:
-    print(-1)
-else:
-    for p in permutations(map(str, range(1, n+1)), n):
-        tmp = " ".join(p)
-        if now == tmp:
-            print(prev)
-            break
-        prev = tmp
+#if now == prev:
+#    print(-1)
+#else:
+#    for p in permutations(map(str, range(1, n+1)), n):
+#        if now == p:
+#            print(" ".join(prev))
+#            break
+#        prev = p
+
+for p in permutations(map(str, range(1, n+1)), n):
+    print(p)
