581601597909b3b496f2e526cf372b6ca35a40e1
guswnsakvk <guswnsakvk1@naver.com>
2023-07-08 21:21:46 +0900

    실패

--- BackJoon/2812.py
+++ BackJoon/2812.py
@@ -1,30 +1,31 @@
 n, k = map(int, input().split())
 
 lst = list(map(int, input()))
+
 stack = []
+tmp = []
+max_num = max(lst)
+max_len = n-k
 
-answer = []
-count = 0
-check = False
-num = 0
-
-for i in range(n):
+for i in range(n):  
   if k == 0:
-    answer.append(lst[i])
-  
-  stack.append(lst[i])
-  
-  if stack and len(stack) == k+1 and k != 0:
-    num = max(stack)
-    check = True
-  
-  if check:
-    for i in stack:
-      if i == num:
-        answer.append(num)
-      else:
-        k -= 1
-    stack = []
-    check = False
+    if len(tmp) <= max_len:
+      tmp.append(lst[i])
+    continue
 
-print("".join(map(str,answer)))
+  if lst[i] == max_num:
+    while stack and k != 0:
+        min_num = min(stack)
+        stack.remove(min_num)
+        k -= 1
+    if i+1 >= n:
+      max_num = max(lst[i:])
+    else:
+      max_num = max(lst[i+1:])
+    tmp.extend(stack)
+    tmp.append(lst[i])
+    stack = []
+  else:
+    stack.append(lst[i])
+
+print("".join(map(str, tmp[:max_len])))
