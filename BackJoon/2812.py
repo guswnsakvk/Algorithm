54f7f93dbec54d4824acab7a20ba7f27c5773566
guswnsakvk <guswnsakvk1@naver.com>
2023-07-09 22:33:01 +0900

    크게 만들기 : 스택 사용

--- BackJoon/2812.py
+++ BackJoon/2812.py
@@ -3,29 +3,27 @@ n, k = map(int, input().split())
 lst = list(map(int, input()))
 
 stack = []
-tmp = []
-max_num = max(lst)
-max_len = n-k
+max_num = lst[0]
+count = k
 
-for i in range(n):  
-  if k == 0:
-    if len(tmp) <= max_len:
-      tmp.append(lst[i])
+for i in range(n):
+  if count == 0:
+    stack.append(lst[i])
     continue
-
-  if lst[i] == max_num:
-    while stack and k != 0:
-        min_num = min(stack)
-        stack.remove(min_num)
-        k -= 1
-    if i+1 >= n:
-      max_num = max(lst[i:])
-    else:
-      max_num = max(lst[i+1:])
-    tmp.extend(stack)
-    tmp.append(lst[i])
-    stack = []
+  
+  if not stack:
+    stack.append(lst[i])
   else:
+    if lst[i] > max_num:
+      max_num = lst[i]
+
+    while stack and count != 0 and stack[-1] < max_num:
+      if stack[-1] < lst[i]:
+        count -= 1
+        stack.pop()
+      else:
+        break
+
     stack.append(lst[i])
 
-print("".join(map(str, tmp[:max_len])))
+print("".join(map(str, stack[:n-k])))
