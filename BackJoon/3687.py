aecd350439d734b9dcaa966397cf571aad289d62
guswnsakvk <guswnsakvk1@naver.com>
2023-08-12 16:11:12 +0900

    성냥개비 : dp 사용

--- BackJoon/3687.py
+++ BackJoon/3687.py
@@ -1,26 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+N = int(input())
 max_dp = [0, 0, 1, 7, 11, 71]
-min_dp = [0, 0, 1, 7, 4, 2, 6, 8, 10, 18, 22, 20]
+min_dp = [0, 0, 1, 7, 4, 2, 6, 8, 10, 18, 22, 20, 28, 68, 88, 108, 188, 200]
 
 for i in range(6, 101):
     max_dp.append(max_dp[i - 2] * 10 + 1)
 
-num = '20'
+num = '200'
 n = 0
-place = 1
-for i in range(11, 101):
+place = 2
+tmp = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+for i in range(18, 101):
     if '2' in num:
         if '0' in num:
-            num = str(int(num) + (8 * 10 ** n))
+            num = str(int(num) + (8 * (10 ** n)))
             min_dp.append(int(num))
             n += 1
         else:
             num = num.replace('2', '6')
             min_dp.append(int(num))
-            n = 0
+            n = tmp.pop(0)
     elif '6' in num:
         num = num.replace('6', '8')
         min_dp.append(int(num))
@@ -32,9 +33,9 @@ for i in range(11, 101):
             num = num.replace('0', '8')
             min_dp.append(int(num))
         else:
-            num = '2' + '0' * place 
+            num = '200' + '8' * (place-2)
             min_dp.append(int(num))
 
-for _ in range(n):
+for _ in range(N):
     cnt = int(input())
     print(min_dp[cnt], max_dp[cnt])
