792878d5581cec80615cb939023e6db3249ff803
guswnsakvk <guswnsakvk1@naver.com>
2023-08-29 21:49:53 +0900

    토너먼트 만들기

--- BackJoon/2262.py
+++ BackJoon/2262.py
@@ -1,29 +1,20 @@
-import heapq
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 rank = list(map(int, input().split()))
-rank_len = n
 answer = 0
 
-for _ in range(n-1):
-  heap = []
-  cnt = abs(rank[0] - rank[1])
-  heapq.heappush(heap, max(rank[0], rank[1]) * -1)
+for i in range(n-1):
+  target = max(rank)
+  idx = rank.index(target)
 
-  for i in range(1, rank_len-1):
-    tmp = abs(rank[i] - rank[i+1])
+  if idx == 0:
+    answer += target - rank[idx+1]
+  elif idx == len(rank)-1:
+    answer += target - rank[idx-1]
+  else:
+    answer += min(target - rank[idx+1], target - rank[idx-1])
 
-    if cnt > tmp:
-      heap.clear()
-      cnt = tmp
-      heapq.heappush(heap, max(rank[i], rank[i+1]) * -1)
-    elif cnt == tmp:
-      heapq.heappush(heap, max(rank[i], rank[i+1]) * -1)
-    
-  rank_len -= 1
-  rank.remove(heapq.heappop(heap) * -1)
-  answer += cnt
-
+  rank.remove(target)
 print(answer)
