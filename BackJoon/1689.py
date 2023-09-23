9f7d78886a27d23cd6e1ba431bac3a9b86a862ae
guswnsakvk <guswnsakvk1@naver.com>
2023-09-23 23:12:47 +0900

    겹치는 선분 : heap 풀이 추가

--- BackJoon/1689.py
+++ BackJoon/1689.py
@@ -18,3 +18,29 @@ for x, v in arr:
     ans = max(ans, cnt)
 
 print(ans)
+
+## heap 풀이
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+lines = []
+for _ in range(N):
+    lines.append(list(map(int, input().split())))
+
+lines.sort(key=lambda x : x[0])
+
+heap = []
+heapq.heappush(heap, lines[0][1])
+
+answer = 1
+
+for s, e in lines[1:]:
+    while heap and heap[0] <= s:
+        heapq.heappop(heap)
+    heapq.heappush(heap, e)
+    answer = max(answer, len(heap))
+
+print(answer)
